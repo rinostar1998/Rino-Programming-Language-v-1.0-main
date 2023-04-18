5145391468d144aefd2a33b0bf3e68d1c1b13d6f
rinostar1998 <130912302+rinostar1998@users.noreply.github.com>
2023-04-18 18:05:15 +0000

    hjbjbj

--- Rino.py
+++ Rino.py
@@ -1173,6 +1173,29 @@ class List(Value):
         new_list.elements.append(other)
         return new_list, None
 
+    def subbed_by(self, other):
+        if isinstance(other, List):
+            new_list = self.copy()
+            try:
+                new_list.elements.pop(other.value)
+                return new_list, None
+            except:
+                return None, RTError(other.pos_start, other.pos_end, 
+                 "Why are you trying to remove a non-existent object that's out of bounds!?!?", self.context)
+        else:
+            return None, Value.illegal_operation(self, other)
+        
+    def dived_by(self, other):
+        if isinstance(other, List):
+            new_list = self.copy()
+            try:
+                return self.elements[other.value]
+            except:
+                return None, RTError(other.pos_start, other.pos_end, 
+                 "Why are you trying to remove a non-existent object that's out of bounds!?!?", self.context)
+        else:
+            return None, Value.illegal_operation(self, other)
+
     def multed_by(self, other):
         if isinstance(other, List):
             new_list = self.copy()
