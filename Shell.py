2ceb16d39ab0efeba20cadec3d781a27d917e886
RinostarGames <110748785+RinostarGames@users.noreply.github.com>
2023-04-29 19:15:27 -0700

    fdrgsz

--- Shell.py
+++ Shell.py
@@ -1,35 +1,14 @@
 import Rino
 
-openFile = input("Run file or use terminal inputs? y/n: ")
+while True:
+	text = input('input > ')
+	if text.strip() == "": continue
+	result, error = Rino.run('<stdin>', text)
 
-
-if openFile == "y":
-
-    directory = input("Input name of file and/or directory: ")
-
-    with open(directory) as f:
-        if f'{f.name.split(".")[len(f.name.split(".")) - 1]}' == "rno":
-            text = f.read()
-            result, error = Rino.run('[code_input]', text)
-
-            if(error):
-                print(error.as_string())
-            elif result:
-                if len(result.elements) == 1:
-                    print(repr(result.elements[0]))
-                else:
-                   print(result)
-        else: print("File loader only supports .rno files.")
-else:
-    while True:
-
-        text = input("Input Code: ")
-        result, error = Rino.run('[code_input]', text)
-
-        if(error):
-            print(error.as_string())
-        elif result:
-            if len(result.elements) == 1:
-                print(repr(result.elements[0]))
-            else:
-                print(result)
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
